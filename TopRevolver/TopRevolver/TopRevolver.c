10fbb61735f40c1243a99ac4134baff1fbbd76ed
Yuan Cao <caoyuan@mit.edu>
2015-05-03 12:49:53 -0400

    Updated Top Revolver circuit

--- TopRevolver/TopRevolver/TopRevolver.c
+++ TopRevolver/TopRevolver/TopRevolver.c
@@ -11,12 +11,16 @@
 #include <avr/pgmspace.h>
 int main(void)
 {
-	DDRA |= _BV(PA0) | _BV(PA1);
+	DDRA |= _BV(PA0) | _BV(PA2) | _BV(PA3);
 	PORTA = 0;
-	
+	int counter = 0;
     while(1)
     {
         _delay_ms(5);
-		PORTA ^= _BV(PA0);
+		PORTA ^= _BV(PA2);
+		if(++counter == 200){
+			counter = 0;
+			PORTA ^= _BV(PA0);
+		}
     }
 }
